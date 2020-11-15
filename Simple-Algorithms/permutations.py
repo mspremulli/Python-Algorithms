74c1cdcfcea32c6ff91e75f73416ab85d284226e
Michael Spremulli <michaels.wpi@gmail.com>
2020-11-15 18:47:04 -0500

    permutations

---
+++ Simple-Algorithms/permutations.py
@@ -0,0 +1,10 @@
+from itertools import permutations
+
+words, length = input().split()
+
+perm = list(permutations(words, int(length)))
+perm.sort();
+
+for letters in perm:
+  print("".join(letters))
+
