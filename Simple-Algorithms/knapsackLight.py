9b6dd59b41d70f4e7195de048692d2593b0af60a
Michael Spremulli <michaels.wpi@gmail.com>
2021-02-05 18:42:41 -0500

    knapsack light

---
+++ Simple-Algorithms/knapsackLight.py
@@ -0,0 +1,10 @@
+def knapsackLight(value1, weight1, value2, weight2, maxW):
+    if weight1 + weight2 <= maxW:
+        return value1 + value2
+    if value1 > value2 and weight1 <= maxW:
+        return value1;
+    if weight2 <= maxW:
+        return value2;
+    if weight1 <= maxW:
+        return value1;
+    return 0;    
